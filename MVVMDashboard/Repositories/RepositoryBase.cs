161d35aa67e2c58d926bec887b0b8ca1dc0e8b0b
racconrolly@gmail.com <racconrolly@gmail.com>
2024-05-28 13:31:42 +0300

    Добавьте файлы проекта.

---
+++ MVVMDashboard/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDashboard.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string connectionString;
+        public RepositoryBase()
+        {
+            connectionString = "Server=WIN-F0U2NJDM669\\SQLEXPRESS; Database=MVVMLoginDb; Integrated Security=true; trustservercertificate=True";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
